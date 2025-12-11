0bfd68ade45c979646135475f21cd06a19179c08
Your Name <you@example.com>
2025-12-11 10:10:04 +0100

    instant_make

--- py/app.py
+++ py/app.py
@@ -4,19 +4,22 @@ compiled: main.c helper.c
 """
 
 import sys
+import subprocess as sp
 
 def	main():
 	av = sys.argv
 	ac = len(av)
 	if ac < 3: print("Missing args"); return
+	file_names = [s for s in av[2:] if s[0] != '-']
 	compiled_name = av[1]
-	req_files = ".c ".join(av[2:]) + ".c"
-	comp_line = "\tcc " + ".c ".join(av[2:]) + ".c -o " + compiled_name
+	req_files = ".c ".join(file_names) + ".c"
+	comp_line = "\tcc " + ".c ".join(file_names) + ".c -o " + compiled_name
 	line1 = compiled_name + ": " + req_files
 	line2 = comp_line
 	final = line1 + "\n" + line2
 	print("Makefile written")
 	open("Makefile", "w").write(final)
 	print(final)
+	if "-make" in av: sp.run("make", shell=True)
 
 if __name__ == "__main__":main()
